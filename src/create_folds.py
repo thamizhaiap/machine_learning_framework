4be464e496b04c2ecaaa667fe04cecf1e6ad68c1
tam <thamizhaiap@gmail.com>
2020-03-18 20:58:26 +0000

    train.py code is added

--- src/create_folds.py
+++ src/create_folds.py
@@ -3,12 +3,12 @@ from sklearn import model_selection
 
 if __name__ == "__main__":
     df = pd.read_csv('input/train.csv')
-    df['kfold']=-1
+    df['kfold'] = -1
 
     # what the following function does????
     df = df.sample(frac=1).reset_index(drop=True)
 
-    kf = model_selection.StratifiedKFold(n_splits=5, shuffle=False, random_state=42)
+    kf = model_selection.StratifiedKFold(n_splits=6, shuffle=True, random_state=42)
 
     for fold, (train_idx, val_idx) in enumerate(kf.split(X=df, y=df.target.values)):
         print(len(train_idx), len(val_idx))
--- src/train.py
+++ src/train.py
@@ -1,10 +1,10 @@
-import os
+import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 from sklearn import ensemble
+from sklearn import metrics
 
-TRAINING_DATA = os.environment.get('TRAINING_DATA')
-FOLD = int(os.environment.get('FOLD'))
+FOLD = 0
 
 FOLD_MAPPING = {
     0:[1,2,3,4],
@@ -15,32 +15,43 @@ FOLD_MAPPING = {
 }
 
 if __name__ == "__main__":
-    df = pd.read_csv(TRAINING_DATA)
+    df = pd.read_csv('input/train_folds.csv')
+    
 
-    train_df = df[df.kfold.isin(FOLD_MAPPING.get(FOLD))]
+    train_df = df[df.kfold.isin(FOLD_MAPPING.get(FOLD))].reset_index(drop=True)
     valid_df = df[df.kfold==FOLD]
+    # print(train_df.head())
+    # print(valid_df.head())
 
     ytrain = train_df.target.values
     yvalid = train_df.target.values
-
-
+    # print(len(ytrain))
+    # print(len(yvalid))
+    
     # training and validation data
     train_df = train_df.drop(['id','target','kfold'], axis=1)
-    valid_df = train_df.drop(['id','target','kfold'], axis=1)
+    valid_df = valid_df.drop(['id','target','kfold'], axis=1)
+    # print(train_df.shape)
+    # print(valid_df.shape)
 
     valid_df = valid_df[train_df.columns]
+    # print(valid_df.head())
 
-    label_encoders = []
+    label_encoders = {}
     for c in train_df.columns:
-        lbl = preprocessing.LabelEncoders()
+        lbl = preprocessing.LabelEncoder()
         lbl.fit(train_df[c].values.tolist() + valid_df[c].values.tolist())
         train_df.loc[:, c] = lbl.transform(train_df[c].values.tolist())
         valid_df.loc[:, c] = lbl.transform(valid_df[c].values.tolist())
-        label_encoders.append(c, lbl)
+        label_encoders[c] = lbl
 
+        
     # data is ready to train
 
-    clf = ensemble.RandomForestClassifier(n_jobs=-1, verbose=2)
+    clf = ensemble.RandomForestClassifier(n_estimators=100, n_jobs=-1, verbose=2)
     clf.fit(train_df, ytrain)
     preds = clf.predict_proba(valid_df)[:,1]
     print(preds)
+ 
+
+
