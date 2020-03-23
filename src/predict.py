3c932dc1fa92c10f6e2db5c27e4576b6b4e45d63
tam <thamizhaiap@gmail.com>
2020-03-23 07:18:52 +0000

    prediction added

--- src/predict.py
+++ src/predict.py
@@ -9,39 +9,44 @@ import joblib
 from . import dispatcher
 
 
+def predict(test_data_path, model_type, model_path):
+    
+    test_df = pd.read_csv(test_data_path)
+    
+    test_idx = test_df["id"].values
+    predictions = None
 
-def predict():
-    df_test = pd.read_csv('input/test.csv')
-    df_test.head()
-    # test_idx = df['id'].values
-    # print(test_idx)
-    # predictions = None
+predict(test_data_path="input/test.csv", model_type="rf", model_path=dispatcher.MODELS)
 
     # for FOLD in range(5):
-    #     encoders = joblib.load(os.join('models'), f'{MODEL}_{FOLD}_label_encoder.pkl')
-    #     cols = joblib.load(os.path.join('models', f"{MODEL}_{FOLD}_columns.pks"))
-         
-    #     for c in cols:
-    #         lbl = label_encoders[c]
-    #         df.loc[:,c] = lbl.transform(df[c].values.tolist())
-
-    #     clf = joblib.load(os.path.join('models', f"{MODEL}_{FOLD}.pkl"))
+    #     df = pd.read_csv(test_data_path)
+    #     encoders = joblib.load(os.path.join(model_path, f"{model_type}_{FOLD}_label_encoder.pkl"))
+    #     cols = joblib.load(os.path.join(model_path, f"{model_type}_{FOLD}_columns.pkl"))
         
+    #     for c in encoders:
+    #         lbl = encoders[c]
+    #         df.loc[:, c] = df.loc[:, c].astype(str).fillna("NONE")
+    #         df.loc[:, c] = lbl.transform(df[c].values.tolist())
+        
+    #     clf = joblib.load(os.path.join(model_path, f"{model_type}_{FOLD}.pkl"))
+        
+
     #     df = df[cols]
-    #     preds = clf.predict_proba(df[:,1])
+    #     preds = clf.predict_proba(df)[:, 1]
+
 
     #     if FOLD == 0:
     #         predictions = preds
-
     #     else:
     #         predictions += preds
+    
+    # predictions /= 5
 
-    #     predictions /= 5
+    # sub = pd.DataFrame(np.column_stack((test_idx, predictions)), columns=["id", "target"])
+    # return sub
+    
 
-    #     sub = pd.DataFrame(np.columnsOstack((test_idx, predictions)),columns=['id','predict'])
-    #     joblib.dump(label_encoders, f"models/{MODEL}_label_encoder.pkl")
-    #     joblib.dump(clf, f"models/{MODEL}.pkl")
-    #     joblib.dump(train_df.columns, f"models/{MODEL}_columns.pkl")
-
-
-predict()
+# if __name__ == "__main__":
+#     submission = predict(test_data_path="input/test.csv", model_type="rf", model_path=dispatcher.MODELS/)
+#     submission.loc[:, "id"] = submission.loc[:, "id"].astype(int)
+#     submission.to_csv(f"models/rf_submission.csv", index=False)
