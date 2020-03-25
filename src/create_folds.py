2a8480126082533b9453acaf6525d97c17f67648
tam <thamizhaiap@gmail.com>
2020-03-25 11:15:05 +0000

    create_fold correction

--- src/create_folds.py
+++ src/create_folds.py
@@ -4,17 +4,17 @@ from sklearn import model_selection
 if __name__ == "__main__":
     df = pd.read_csv('input/train.csv')
     df['kfold'] = -1
-
-    # what the following function does????
+    
+    # shuffling the dataframe
     df = df.sample(frac=1).reset_index(drop=True)
     total_split = 5
-    kf = model_selection.StratifiedKFold(n_splits=total_split , shuffle=True, random_state=42)
+
+    kf = model_selection.StratifiedKFold(n_splits=total_split, shuffle=True, random_state=42)
 
     for fold, (train_idx, val_idx) in enumerate(kf.split(X=df, y=df.target.values)):
         df.loc[val_idx, 'kfold'] = fold
         print(len(train_idx), len(val_idx))
-        
 
-
+    # creating csv file       
     df.to_csv('input/train_folds.csv', index=False)
 
